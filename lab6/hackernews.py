cd970c3fe97b925371607eee3f6b5a7e5a95272d
Vengerov Dmitrij <hardbeat34@gmail.com>
2018-03-04 08:45:21 +0300

    first task complete

--- lab6/hackernews.py
+++ lab6/hackernews.py
@@ -4,46 +4,46 @@ from bs4 import BeautifulSoup
 
 
 
-'''
-Example json 
-{'author': 'evo_9',
-  'comments': 0,
-  'points': 1,
-  'title': 'Daily Action – Sign Up to Join the Resistance',
-  'url': 'https://dailyaction.org/'}
-
-'''
-
 
 def get_news(url='https://news.ycombinator.com/newest'):
 
+    news_list = []
+    data = {}
     r = requests.get(url)
+
     page = BeautifulSoup(r.text, 'html.parser')
     tbl_list = page.table.find('table', {'class': 'itemlist'})
     storyTitle = tbl_list.findAll('a', {'class': 'storylink'})
     storyAuthor = tbl_list.findAll('a', {'class': 'hnuser'})
     storyPoints = tbl_list.findAll('span', {'class': 'score'})
     storyComments = tbl_list.findAll('td', {'class': 'subtext'})
-
     for i in range(len(storyTitle)):
+        tempCom = storyComments[i].findAll('a')
+        tempCom = tempCom[len(tempCom)-1].text
+        if(tempCom == 'discuss'):
+            tempCom = int(0)
+        else:
+            tempCom = int(tempCom.replace(u'\xa0', ' ').split(' ')[0])
+        tempPoi = storyPoints[i].text
+        tempPoi = int(tempPoi.replace('&nbsp;','').split(' ')[0])
+        
+        data['author'] = storyAuthor[i].text
+        data['comments'] = tempCom
+        data['points'] = tempPoi
+        data['title'] = storyTitle[i].text
+        data['url'] = storyTitle[i].get('href')
 
-    	tempCom = storyComments[i].findAll('a')
-    	tempCom = tempCom[len(tempCom)-1].text
-    	tempPoi = storyPoints[i].text
-    	
-    	#tempPoi = re.findall('(\d+)',tempPoi)
-    	print(
-    		"name: " + 
-    		storyTitle[i].text + 
-    		"\nauthor: " +
-    		storyAuthor[i].text +
-    		"\nlikes: " +
-			tempPoi +
-    		"\ncomments: " +
-    		tempCom +
-    		"\n"
-    		)
-	
+        news_list.append({
+
+            'author':data['author'],
+            'comments':data['comments'],
+            'points':data['points'],
+            'title':data['title'],
+            'url':data['url']
+
+            })
+    print(news_list[:3])
+    
 
 
 if __name__ == '__main__':
