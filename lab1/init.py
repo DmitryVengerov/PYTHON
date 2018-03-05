ff9f1c13c95bb839fcaab7800629a2cfe9e9e15c
Vengerov Dmitrij <hardbeat34@gmail.com>
2018-03-05 10:51:42 +0300

    new order

--- lab1/init.py
+++ lab1/init.py
@@ -36,9 +36,34 @@ def decrypt_caesar(plaintext):
 	else:
 		print('You must enter only letters');
 
+def encrypt(plaintext, key):
+
+	abc = 'abcdefghijklmnopqrstuvwxyz'
+	text_encrypt = ''
+	i = 0
+	for letra in plaintext:
+		sum = abc.find(letra) + abc.find(key[i % len(key)])
+		node = int(sum) % len(abc)
+		text_encrypt = text_encrypt + str(abc[node])
+		i = i + 1
+	return text_encrypt
+
+def decrypt(plaintext, key):
+
+	abc = 'abcdefghijklmnopqrstuvwxyz'
+	text_encrypt = ''
+	i = 0
+	for letra in plaintext:
+		sum = abc.find(letra) - abc.find(key[i % len(key)])
+		node = int(sum) % len(abc)
+		text_encrypt = text_encrypt + str(abc[node])
+		i = i + 1
+	return text_encrypt
 
 def main():
 	encrypt_caesar(str(input('Encrypted: ')))
 	decrypt_caesar(str(input('Decrypted: ')))
+	print(encrypt(str(input('plaintext a encrypt: ')).lower(), str(input('key: ')).lower()))
+	print(decrypt(str(input('plaintext a desencrypt: ')).lower(), str(input('key: ')).lower()))
 
 main()
--- lab6/hackernews.py
+++ lab6/hackernews.py
@@ -1,50 +1,53 @@
 import requests
-import re
 from bs4 import BeautifulSoup
 
+news_list = []
+data = {}
 
-
-
-def get_news(url='https://news.ycombinator.com/newest'):
-
-    news_list = []
-    data = {}
+def get_news(url='https://news.ycombinator.com/newest', pageCount=1):
     r = requests.get(url)
-
     page = BeautifulSoup(r.text, 'html.parser')
     tbl_list = page.table.find('table', {'class': 'itemlist'})
     storyTitle = tbl_list.findAll('a', {'class': 'storylink'})
     storyAuthor = tbl_list.findAll('a', {'class': 'hnuser'})
     storyPoints = tbl_list.findAll('span', {'class': 'score'})
     storyComments = tbl_list.findAll('td', {'class': 'subtext'})
+
+    # catch get url for other pages
+    moreLink = tbl_list.findAll('a', {'class': 'morelink'})
+    nextPage = url+moreLink[0].get('href').replace('newest', '') 
+
+    print(nextPage)
+
     for i in range(len(storyTitle)):
         tempCom = storyComments[i].findAll('a')
-        tempCom = tempCom[len(tempCom)-1].text
+        tempCom = tempCom[len(tempCom)-1].text    
         if(tempCom == 'discuss'):
             tempCom = int(0)
         else:
             tempCom = int(tempCom.replace(u'\xa0', ' ').split(' ')[0])
         tempPoi = storyPoints[i].text
-        tempPoi = int(tempPoi.replace('&nbsp;','').split(' ')[0])
-        
+        tempPoi = int(tempPoi.replace('&nbsp;', '').split(' ')[0])
         data['author'] = storyAuthor[i].text
         data['comments'] = tempCom
         data['points'] = tempPoi
         data['title'] = storyTitle[i].text
         data['url'] = storyTitle[i].get('href')
-
         news_list.append({
+            'author': data['author'],
+            'comments': data['comments'],
+            'points': data['points'],
+            'title': data['title'],
+            'url': data['url']
+        })
+    return news_list
 
-            'author':data['author'],
-            'comments':data['comments'],
-            'points':data['points'],
-            'title':data['title'],
-            'url':data['url']
-
-            })
-    print(news_list[:3])
-    
-
+def next_page(url,page):
+    return url
 
 if __name__ == '__main__':
-    get_news()
+    print(get_news())
+    #print(get_news('https://news.ycombinator.com/newest?next=16513818&n=31'))
+    #print(get_news('https://news.ycombinator.com/newest?next=16513229&n=61'))
+
+    
