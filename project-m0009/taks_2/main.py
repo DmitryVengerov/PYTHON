8df2237387699c477368381259f527320ce2692f
vengerov <hardbeat34@gmail.com>
2019-09-14 16:04:01 +0300

    wip

--- project-m0009/taks_2/main.py
+++ project-m0009/taks_2/main.py
@@ -1,4 +1,5 @@
 from skimage import *
+from skimage.exposure import histogram
 import os
 import numpy as np
 import collections
@@ -13,7 +14,7 @@ class image_service:
         pass
 
     def save_image(self, name, image):
-        PATH = './lab_1_pic/'
+        PATH = './lab_2_pic/'
 
         if not os.path.isdir(PATH):
             os.makedirs(PATH)
@@ -30,38 +31,97 @@ class image_service:
         plt.show()
 
 # FIRST TASK
-class Lab2_first(image_service):
+class LabTwo(image_service):
     def __init__(self):
-        self.simple_image = self.read_file('simple.jpg')
-
-    def call(self):
-    	self.linear_stretch()
-
-
-    def map_diff(self, image1, image2):
-    	pass
-    
-    def linear_stretch(self):
-    	k = round(self.simple_image.size * .05)
-    	print(self.simple_image.ravel())
-    	print(k)
-    	pass
-
-# SECOND TASK
-class Lab2_second(image_service):
-	def __init__(self):
-		pass
-
-# THIRD TASK
-class Lab2_third(image_service):
-	def __init__(self):
-		pass
-
-# FOUR TASK
-class Lab2_four(image_service):
-	def __init__(self):
-		pass
+        self.image = self.read_file('simple.jpg')
+        self.linearImage = self.read_file('./lab_2_pic/linear_stretch.jpg')
+        self.pct = 10
         
+    def call(self):
+        # self.save_image('gray_simple.jpg', self.make_grayscale())
+        # self.makeHistogram()
 
+        # self.save_image('linear_stretch.jpg', self.linear_stretch())
+        # self.image = self.read_file('./lab_2_pic/linear_stretch.jpg')
+        # self.makeHistogram()
+
+        # self.save_image('diff.jpg', self.difference())
+        # self.save_image('docked.jpg', self.docking())
+
+        # self.save_image('exp_simple.jpg', self.eachChannel())
+        # self.image = self.read_file('./lab_2_pic/exp_simple.jpg')
+        # self.makeHistogram()
+
+        self.save_image('gray_word_simple.jpg', self.grayWorld())
+
+    # тут серим
+    def make_grayscale(self):
+        r_channel = self.image[:,:,0]
+        return r_channel    
+
+    # тут делаем гистограмму
+    def makeHistogram(self):
+        hist, bins = np.histogram(self.image.flatten(), 256, [0, 256])
+        plt.hist(self.image.flatten(), 256, [0, 256], color='black')
+        plt.xlim([0, 256])
+        plt.show()
+
+    # тут робастое растяжение 
+    def linear_stretch(self):
+        y, x = histogram(self.image)
+
+        xMin = int(np.percentile(x, self.pct))
+        xMax = int(np.percentile(x, 100 - self.pct))
+
+        linearImage = np.array([np.array([
+                np.clip(np.uint8((px - xMin) * (255/(xMax-xMin))), 0, 255)
+                for px in row]) for row in self.image])
+        
+        return linearImage
+    
+    # карта разности
+    def difference(self):
+        return np.abs(self.image - self.linearImage)
+
+    # стыковка изображения
+    def docking(self):
+        image = self.image
+        linearImage = self.linearImage
+
+        for px in range(len(image) // 2, len(linearImage)):
+            image[px] = linearImage[px]
+
+        return image.astype('uint8')  
+
+    def exp(self, image):
+
+        y, x = histogram(image)
+        
+        xMin = int(np.percentile(x, 0))
+        xMax = int(np.percentile(x, 100))
+
+        image = np.array([np.array([np.uint8((px - xMin) * (255 / (xMax - xMin))) for px in row]) for row in image])
+
+        return image
+
+    def eachChannel(self):
+        image = self.image
+
+        r = self.exp(image[:, :, 0])
+        g = self.exp(image[:, :, 1])
+        b = self.exp(image[:, :, 2])
+
+        return np.dstack((r, g, b))
+
+    def grayWorld(self):
+        av = np.mean(self.image)
+
+        r = np.clip((av / np.mean(self.image[:, :, 0])) * self.image[:, :, 0], 0, 255)
+        g = np.clip((av / np.mean(self.image[:, :, 1])) * self.image[:, :, 1], 0, 255)
+        b = np.clip((av / np.mean(self.image[:, :, 2])) * self.image[:, :, 1], 0, 255)
+
+        return np.dstack((r, g, b)).astype('uint8')
+
+    
 if __name__ == '__main__':
-    lab_two = Lab2_first().call()
+    lab_two = LabTwo().call()
