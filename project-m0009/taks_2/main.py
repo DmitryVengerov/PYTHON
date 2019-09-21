b5b0b7181f6685d7f3d91f117260576f5b20a800
vengerov <hardbeat34@gmail.com>
2019-09-21 17:44:37 +0300

    add pictures

--- project-m0009/taks_2/main.py
+++ project-m0009/taks_2/main.py
@@ -1,14 +1,14 @@
 from skimage import *
 from skimage.exposure import histogram
+from skimage.util import random_noise
+from scipy.ndimage.filters import median_filter
+from PIL import Image, ImageFilter, ImageChops
 import os
 import numpy as np
 import collections
 import warnings
 import matplotlib.pyplot as plt
 
-# save_image(name, image)
-# read_file(name)
-# get_histogramm(image)
 class image_service:
     def __init__(self):
         pass
@@ -35,14 +35,21 @@ class LabTwo(image_service):
     def __init__(self):
         self.image = self.read_file('simple.jpg')
         self.linearImage = self.read_file('./lab_2_pic/linear_stretch.jpg')
+        self.grayImage = self.read_file('./lab_2_pic/gray_simple.jpg')
+        self.spImage = self.read_file('./lab_2_pic/sp_gray_simple.jpg')
+
+        self.spGrayImage = self.read_file('./lab_2_pic/sp_gray_simple.jpg')
+        self.spMedianImage = self.read_file('./lab_2_pic/sp_median_filter.jpg')
+
         self.pct = 10
+        self.kernel = np.array([[1/9,1/9,1/9], [1/9,1/9,1/9], [1/9,1/9,1/9]])
         
     def call(self):
-        # self.save_image('gray_simple.jpg', self.make_grayscale())
+        # self.save_image('gray_simple.jpg', self.makeGrayscale())
         # self.makeHistogram()
 
-        # self.save_image('linear_stretch.jpg', self.linear_stretch())
-        # self.image = self.read_file('./lab_2_pic/linear_stretch.jpg')
+        # self.save_image('linearStretch.jpg', self.linearStretch())
+        # self.image = self.read_file('./lab_2_pic/linearStretch.jpg')
         # self.makeHistogram()
 
         # self.save_image('diff.jpg', self.difference())
@@ -52,10 +59,19 @@ class LabTwo(image_service):
         # self.image = self.read_file('./lab_2_pic/exp_simple.jpg')
         # self.makeHistogram()
 
-        self.save_image('gray_word_simple.jpg', self.grayWorld())
+        # self.save_image('gray_word_simple.jpg', self.grayWorld())
+
+        # self.save_image('docking_gray_wold.jpg', self.dockingGrayWorld())
+
+        # self.save_image('spGray_simple.jpg', self.spGray())
+        # self.save_image('sp_median_filter.jpg', self.medianFilter())
+        
+        # self.save_image('gray_diff_filtered.jpg', self.differenceFilter())
+        # self.save_image('grat_convolution.jpg', self.convolution())
+        self.average()
 
     # тут серим
-    def make_grayscale(self):
+    def makeGrayscale(self):
         r_channel = self.image[:,:,0]
         return r_channel    
 
@@ -67,7 +83,7 @@ class LabTwo(image_service):
         plt.show()
 
     # тут робастое растяжение 
-    def linear_stretch(self):
+    def linearStretch(self):
         y, x = histogram(self.image)
 
         xMin = int(np.percentile(x, self.pct))
@@ -82,6 +98,9 @@ class LabTwo(image_service):
     # карта разности
     def difference(self):
         return np.abs(self.image - self.linearImage)
+    
+    def differenceFilter(self):
+        return np.abs(img_as_float(self.spMedianImage), img_as_float(self.spGrayImage))
 
     # стыковка изображения
     def docking(self):
@@ -122,6 +141,42 @@ class LabTwo(image_service):
 
         return np.dstack((r, g, b)).astype('uint8')
 
+        # стыковка изображения
     
+    def dockingGrayWorld(self):
+        image = self.image
+        Image = self.grayWorld()
+
+        for px in range(len(image) // 2, len(Image)):
+            image[px] = Image[px]
+
+        return image.astype('uint8')  
+    
+    def spGray(self): 
+        return random_noise(self.grayImage, mode='s&p', seed=None, clip=True)
+    
+    def medianFilter(self):
+        return median_filter(self.spImage, 3)
+
+    # 2.4
+    def convolution(self):
+        kernel = np.flipud(np.fliplr(self.kernel))
+        output = np.zeros_like(self.grayImage)
+        img_padded = np.zeros((self.grayImage.shape[0] + 2, self.grayImage.shape[1] + 2))
+        img_padded[1 : -1, 1 : -1] = self.grayImage
+
+        for x in range(self.grayImage.shape[1]):
+            for y in range(self.grayImage.shape[0]):
+                output[y, x] = (kernel * img_padded[y: y+3, x: x+3]).sum()
+
+        return output
+
+    def average(self):
+        # pass
+        self.save_image('average_gray.jpg',self.grayImage.filter(ImageFilter.SMOOTH))
+
+
+
+
 if __name__ == '__main__':
     lab_two = LabTwo().call()
