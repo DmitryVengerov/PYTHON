1301260b770c82713b09baf7a592e2ef4674f8ea
vengerov <hardbeat34@gmail.com>
2019-10-08 08:43:39 +0300

    change spectogramms

--- project-m0009/task_4/main.py
+++ project-m0009/task_4/main.py
@@ -9,6 +9,7 @@ import wave
 import struct 
 import numpy as np
 
+
 class AudioGenerateService:
     def __init__(self):
         self.audio = []
@@ -68,34 +69,102 @@ class AudioHistogram:
         combine_wav.export('combine.wav', format = 'wav')
         pass        
 
-    def create_frequency_spectrogram(self, audioPath):
-        pass
-        sampleRate, samples = wav.read(audioPath)
-        frequencies, times, spectogram = signal.spectrogram(samples, sampleRate)
-        plt.pcolormesh(times * 1000, frequencies, 10 * np.log10(spectogram))
-        plt.ylabel('Frequency [Hz]')
-        plt.xlabel('Time [Ms]')
-        plt.show()
-        pass
+    # def create_frequency_spectrogram(self, audioPath):
+    #     pass
+    #     sampleRate, samples = wav.read(audioPath)
+    #     frequencies, times, spectogram = signal.spectrogram(samples, sampleRate)
+    #     plt.pcolormesh(times * 1000, frequencies, 10 * np.log10(spectogram))
+    #     plt.ylabel('Frequency [Hz]')
+    #     plt.xlabel('Time [Ms]')
+    #     plt.show()
+    #     pass
 
-    def create_amplitude_spectogramm(self, audioPath):
-        sample_rate, samples = wav.read(audioPath)
+    # def create_amplitude_spectogramm(self, audioPath):
+    #     sample_rate, samples = wav.read(audioPath)
 
-        samples = samples / (2. ** 15)
-        sample_points = float(samples.shape[0])
-        mono_audio = samples[:, 0]
+    #     samples = samples / (2. ** 15)
+    #     sample_points = float(samples.shape[0])
+    #     mono_audio = samples[:, 0]
 
-        times = np.arange(0, sample_points, 1) / sample_rate * 1000
+    #     times = np.arange(0, sample_points, 1) / sample_rate * 1000
 
-        plt.plot(times, mono_audio, color='R')
-        plt.ylabel('Amplitude')
-        plt.xlabel('Time [ms]')
-        plt.show()
+    #     plt.plot(times, mono_audio, color='R')
+    #     plt.ylabel('Amplitude')
+    #     plt.xlabel('Time [ms]')
+    #     plt.show()
+
+    def stft(self, sig, frameSize, overlapFac=0.5, window=np.hanning):
+        win = window(frameSize)
+        hopSize = int(frameSize - np.floor(overlapFac * frameSize))
+        
+        samples = np.append(sig, np.zeros(int(np.floor(frameSize/2.0))))
+        cols = int(np.ceil( (len(samples) - frameSize) / float(hopSize)) + 1)
+        samples = np.append(samples, np.zeros(frameSize))
+        
+        frames = stride_tricks.as_strided(samples, shape=(cols, frameSize), strides=(samples.strides[0]*hopSize, samples.strides[0])).copy()
+        frames *= win
+        
+        return np.fft.rfft(frames)    
+        
+    def logscale_spec(self, spec, sr=44100, factor=20.):
+        timebins, freqbins = np.shape(spec)
+
+        scale = np.linspace(0, 1, freqbins) ** factor
+        scale *= (freqbins-1)/max(scale)
+        scale = np.unique(np.round(scale))
+        
+        newspec = np.complex128(np.zeros([timebins, len(scale)]))
+        for i in range(0, len(scale)):
+            if i == len(scale)-1:
+                newspec[:,i] = np.sum(spec[:,int(scale[i]):], axis=1)
+            else:        
+                newspec[:,i] = np.sum(spec[:,int(scale[i]):int(scale[i+1])], axis=1)
+        
+        allfreqs = np.abs(np.fft.fftfreq(freqbins*2, 1./sr)[:freqbins+1])
+        freqs = []
+        for i in range(0, len(scale)):
+            if i == len(scale)-1:
+                freqs += [np.mean(allfreqs[int(scale[i]):])]
+            else:
+                freqs += [np.mean(allfreqs[int(scale[i]):int(scale[i+1])])]
+        
+        return newspec, freqs
+
+    def plotstft(self, audiopath, binsize=2**10, plotpath=None, colormap="jet"):
+        print(audiopath)
+        samplerate, samples = wav.read(audiopath)
+        s = self.stft(samples, binsize)
+        
+        sshow, freq = self.logscale_spec(s, factor=1.0, sr=samplerate)
+        ims = 20.*np.log10(np.abs(sshow)/10e-6)
+        
+        timebins, freqbins = np.shape(ims)
+        
+        plt.figure(figsize=(15, 7.5))
+        plt.imshow(np.transpose(ims), origin="lower", aspect="auto", cmap=colormap, interpolation="none")
+        plt.colorbar()
+
+        plt.xlabel("time (s)")
+        plt.ylabel("frequency (hz)")
+        plt.xlim([0, timebins-1])
+        plt.ylim([0, freqbins])
+
+        xlocs = np.float32(np.linspace(0, timebins-1, 5))
+        plt.xticks(xlocs, ["%.02f" % l for l in ((xlocs*len(samples)/timebins)+(0.5*binsize))/samplerate])
+        ylocs = np.int16(np.round(np.linspace(0, freqbins-1, 10)))
+        plt.yticks(ylocs, ["%.02f" % freq[i] for i in ylocs])
+        
+        if plotpath:
+            plt.savefig(plotpath, bbox_inches="tight")
+        else:
+            plt.show()
+            
+        plt.clf()
 
     def call(self):
-        self.create_frequency_spectrogram("output.wav")
-        self.create_amplitude_spectogramm("girei.wav")
-        self.create_amplitude_spectogramm("combine.wav")
+        self.plotstft('girei.wav')
+        self.plotstft('output.wav')
+        self.plotstft('combine.wav')
 
         pass
 
